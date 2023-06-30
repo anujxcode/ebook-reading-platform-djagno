c5975a212b9f72f0190041715cef5b39ac6ac79c
Anuj Rathore <106525163+anujxcode@users.noreply.github.com>
2023-06-30 12:36:29 +0530

    project del

--- OnBooks/emailauth.py
+++
@@ -1,5 +0,0 @@
-emailSubject ="Reset Login Password"
-emailBody = ''' we recived requested to reset your 
-                    login password So we send a 4 digt verification otp to reset your onbook account password.
-                    Please do not share your One Time Password(OTP)with anyone.
-'''
